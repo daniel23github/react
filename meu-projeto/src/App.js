6d70834e111495d38dea9b1509ca42533299bbf0
daniel23github <daniel2005fevereiro@gmail.com>
2023-04-23 15:11:04 -0300

    fragments

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -4,6 +4,7 @@ import HelloWorld from './components/HelloWorld';
 import Pessoa from './components/Pessoa';
 import SayMyName from './components/SayMyName'
 import Frase from './components/Frase';
+import List from './components/List';
 
 
 function App() {
@@ -17,6 +18,7 @@ function App() {
       <Frase/>
       <SayMyName nome={nome}/>
       <Pessoa nome={nome} idade="18" profissao="Estudante" foto="https://via.placeholder.com/150"/>
+      <List/>
     </div>
   );
 }
