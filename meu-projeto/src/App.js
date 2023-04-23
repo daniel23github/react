34b2e9e1afb949e9815e9ba04fa9d02f8f5607ab
daniel23github <daniel2005fevereiro@gmail.com>
2023-04-22 21:26:55 -0300

    introdução jsx

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -2,22 +2,23 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const name = "Daniel"
+
+  const newname = name.toUpperCase()
+
+  function sum(a, b) {
+    return a + b
+  }
+
+  const url = 'https://via.placeholder.com/150'
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Alterando o JSX</h2>
+      <p>Olá, {newname}</p>
+      <p> Soma: {sum(1, 2)}</p>
+      <img src={url} alt="Minha imagem"/>
     </div>
   );
 }
