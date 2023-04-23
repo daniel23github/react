d04fe7300f65317c055ac3cdfbd12fd7a9a7ad1c
daniel23github <daniel2005fevereiro@gmail.com>
2023-04-23 15:02:03 -0300

    css em components

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import HelloWorld from './components/HelloWorld';
 import Pessoa from './components/Pessoa';
 import SayMyName from './components/SayMyName'
+import Frase from './components/Frase';
 
 
 function App() {
@@ -11,6 +12,9 @@ function App() {
 
   return (
     <div className="App">
+      <h1>Testando CSS</h1>
+      <Frase/>
+      <Frase/>
       <SayMyName nome={nome}/>
       <Pessoa nome={nome} idade="18" profissao="Estudante" foto="https://via.placeholder.com/150"/>
     </div>
