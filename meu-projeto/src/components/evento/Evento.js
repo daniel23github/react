2c7b0f8e0d359eb2fe4fe9fa3b5757abd7b94235
daniel23github <daniel2005fevereiro@gmail.com>
2023-05-01 21:36:05 -0300

    Passar eventos por props

--- meu-projeto/src/components/evento/Evento.js
+++ meu-projeto/src/components/evento/Evento.js
@@ -1,12 +1,20 @@
+import Button from "./Button"
+
 function Evento({numero}) {
     
     function meuEvento() {
         console.log(`Fui ativado! ${numero}`)
     }
     
+    function segundoEvento() {
+        console.log('Ativando o segundo Evento!')
+    }
+
     return (
         <div>
             <p>Clique para disparar um evento: </p>
+            <Button event={meuEvento} text="Primeiro Evento"/>
+            <Button event={segundoEvento} text="Segundo Evento"/>
             <button onClick={meuEvento}>Ativar!</button>
         </div>
     )
