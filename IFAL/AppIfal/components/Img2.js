f81349029c896afc71bf75be71932261affbc0e9
daniel23github <129347245+daniel23github@users.noreply.github.com>
2023-07-03 08:53:33 -0300

    corrigido

---
+++ IFAL/AppIfal/components/Img2.js
@@ -0,0 +1,12 @@
+import styles from './Img2.module.css'
+
+function Img2(props) {
+    return (
+        <div>
+            <p>Essa é uma imagem de {props.tipo}</p>
+            <img src={props.src}></img>
+        </div>
+    )
+}
+
+export default Img2
